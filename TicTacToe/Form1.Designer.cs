1170ecb18bf0f8a1b7602250d4b19b67f08a2865
Hepiix <maciej.szustakowski71@gmail.com>
2023-05-15 12:15:46 +0200

    first commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.Label();
+            this.ButtonsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.ColumnCount = 3;
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.ButtonsPanel.Controls.Add(this.button9, 2, 2);
+            this.ButtonsPanel.Controls.Add(this.button8, 1, 2);
+            this.ButtonsPanel.Controls.Add(this.button7, 0, 2);
+            this.ButtonsPanel.Controls.Add(this.button6, 2, 1);
+            this.ButtonsPanel.Controls.Add(this.button5, 1, 1);
+            this.ButtonsPanel.Controls.Add(this.button4, 0, 1);
+            this.ButtonsPanel.Controls.Add(this.button3, 2, 0);
+            this.ButtonsPanel.Controls.Add(this.button2, 1, 0);
+            this.ButtonsPanel.Controls.Add(this.button1, 0, 0);
+            this.ButtonsPanel.Location = new System.Drawing.Point(12, 12);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.RowCount = 3;
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.ButtonsPanel.Size = new System.Drawing.Size(189, 182);
+            this.ButtonsPanel.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(128, 123);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(58, 56);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(65, 123);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 56);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(3, 123);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 56);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(128, 63);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 54);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(65, 63);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 54);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(3, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 54);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(128, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(65, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.Location = new System.Drawing.Point(9, 245);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(0, 13);
+            this.Output.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 295);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.ButtonsPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ButtonsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel ButtonsPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        List<string> fields = CreateEmptyFields();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Same event for buttons
+            button1.Click += new EventHandler(multiBtnClick);
+            button2.Click += new EventHandler(multiBtnClick);
+            button3.Click += new EventHandler(multiBtnClick);
+            button4.Click += new EventHandler(multiBtnClick);
+            button5.Click += new EventHandler(multiBtnClick);
+            button6.Click += new EventHandler(multiBtnClick);
+            button7.Click += new EventHandler(multiBtnClick);
+            button8.Click += new EventHandler(multiBtnClick);
+            button9.Click += new EventHandler(multiBtnClick);
+        }
+
+        private void multiBtnClick(object sender, EventArgs e)
+        {
+            // Get clicked button
+            Button clickedButton = (Button)sender;
+
+            // check if field is empty
+            if (fields[clickedButton.TabIndex] != "-")
+            {
+                Output.Text = "Niewłaściwy ruch!";
+            }
+            else
+            {
+                clickedButton.Text = "O";
+                fields[clickedButton.TabIndex] = "O";
+                if (CheckDraw())
+                {
+                    EndGame("draw");
+                }
+                else
+                {
+                    PcTurn();
+                }
+                
+            }
+            if (CheckWinnerPlayer())
+            {
+                EndGame("player");
+            }
+
+            
+        }
+
+        // Create empty lists
+        private static List<string> CreateEmptyFields()
+        {
+            List<string> fields = new List<string>();
+            for (int i = 0; i < 9; i++)
+            {
+                fields.Add("-");
+            }
+            Console.WriteLine(fields[8]);
+            return fields;
+        }
+
+        private bool CheckWinnerPlayer()
+        {
+            // 123 fields
+            if (fields[0] == "O" && fields[1] == "O" && fields[2] == "O")
+                return true;
+            // 456 fields
+            else if (fields[3] == "O" && fields[4] == "O" && fields[5] == "O")
+                return true;
+            // 789 fields
+            else if (fields[6] == "O" && fields[7] == "O" && fields[8] == "O")
+                return true;
+            // 147 fields
+            else if (fields[0] == "O" && fields[3] == "O" && fields[6] == "O")
+                return true;
+            // 258 fields
+            else if (fields[1] == "O" && fields[4] == "O" && fields[7] == "O")
+                return true;
+            // 369 fields
+            else if (fields[2] == "O" && fields[5] == "O" && fields[8] == "O")
+                return true;
+            // 159 fields
+            else if (fields[0] == "O" && fields[4] == "O" && fields[8] == "O")
+                return true;
+            // 357 fields
+            else if (fields[2] == "O" && fields[4] == "O" && fields[6] == "O")
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckWinnerPC()
+        {
+            // 123 fields
+            if (fields[0] == "X" && fields[1] == "X" && fields[2] == "X")
+                return true;
+            // 456 fields
+            else if (fields[3] == "X" && fields[4] == "X" && fields[5] == "X")
+                return true;
+            // 789 fields
+            else if (fields[6] == "X" && fields[7] == "X" && fields[8] == "X")
+                return true;
+            // 147 fields
+            else if (fields[0] == "X" && fields[3] == "X" && fields[6] == "X")
+                return true;
+            // 258 fields
+            else if (fields[1] == "X" && fields[4] == "X" && fields[7] == "X")
+                return true;
+            // 369 fields
+            else if (fields[2] == "X" && fields[5] == "X" && fields[8] == "X")
+                return true;
+            // 159 fields
+            else if (fields[0] == "X" && fields[4] == "X" && fields[8] == "X")
+                return true;
+            // 357 fields
+            else if (fields[2] == "X" && fields[4] == "X" && fields[6] == "X")
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckDraw()
+        {
+            if (fields.Contains("-"))
+                return false;
+            else
+                return true;
+        }
+
+        // Pc Turn
+        private void PcTurn()
+        {
+            
+
+            
+
+        Start:
+            Random rnd = new Random();
+            int choice = rnd.Next(8);
+
+            if (fields[choice] != "-")
+            {
+                goto Start;
+            }
+            else
+            {
+                fields[choice] = "X";
+                RefreshBoard();
+            }
+
+            if (CheckWinnerPC())
+            {
+                EndGame("pc");
+            }
+
+            if (CheckDraw())
+            {
+                EndGame("draw");
+            }
+        }
+
+        // Refresh board after pc turn
+        private void RefreshBoard()
+        {
+            button1.Text = fields[0];
+            button2.Text = fields[1];
+            button3.Text = fields[2];
+            button4.Text = fields[3];
+            button5.Text = fields[4];
+            button6.Text = fields[5];
+            button7.Text = fields[6];
+            button8.Text = fields[7];
+            button9.Text = fields[8];
+        }
+
+        // 
+        private void EndGame(string winner)
+        {
+            if (winner == "player")
+            {
+                Output.Text = "Wygrałeś!";
+            }
+            else if (winner == "draw")
+            {
+                Output.Text = "Remis";
+            }
+            else
+            {
+                Output.Text = "Przegrałeś :(";
+            }
+        }
+    }
+}
